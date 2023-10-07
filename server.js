0cc9a6954ca8b7dec2d5b9309556ad8ee2bd6561
Amanbeko <amanbekovnurmakhanbet@gmail.com>
2023-10-07 23:06:24 +0600

    Added login and register page

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.set("view engine", "ejs")
 app.get('/' , (req , res) => {
     res.render('index')
 })
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+app.get('/profile', (req, res) => {
+    res.render('profile')
+})
+app.get('/admin', (req, res) => {
+    res.render('adminProfile')
+})
 
 const PORT = 8000
 app.listen(PORT, () => {

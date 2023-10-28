b92487e2d6321af9a7081526e93ae41b1ae366f3
Amanbeko <amanbekovnurmakhanbet@gmail.com>
2023-10-28 22:36:16 +0600

    added Countries in edit,new

--- server/pages/router.js
+++ server/pages/router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Genres = require('../Genres/Genres')
+const Country = require('../Country/Country')
 
 router.get('/' , async (req , res) => {
     const allGenres = await Genres.find()
@@ -12,19 +13,23 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
     res.render('register')
 })
-router.get('/profile', (req, res) => {
-    res.render('profile')
+router.get('/profile', async(req, res) => {
+    const allGenres = await Genres.find()
+    res.render('profile', {genres: allGenres})
 })
-router.get('/admin', (req, res) => {
-    res.render('adminProfile')
+router.get('/admin', async(req, res) => {
+    const allGenres = await Genres.find()
+    res.render('adminProfile', {genres: allGenres})
 })
 router.get('/new', async(req, res) => {
     const allGenres = await Genres.find()
-    res.render('newFilm', {genres: allGenres})
+    const allCountries = await Country.find()
+    res.render('newFilm', {genres: allGenres, countries: allCountries})
 })
 router.get('/edit', async(req, res) => {
     const allGenres = await Genres.find()
-    res.render('editFilm', {genres: allGenres})
+    const allCountries = await Country.find()
+    res.render('editFilm', {genres: allGenres, countries: allCountries})
 })
 
 module.exports = router
